afff5670dfb31174b89fa56c044c862af548080a
zhangyunqiang2019 <2644888555@qq.com>
2019-08-28 16:32:21 +0800

    0808pm

--- PropertyManagement01/src/main/java/com/neusoft/wuyemis/admin/service/impl/UserInfoServiceImpl.java
+++ PropertyManagement01/src/main/java/com/neusoft/wuyemis/admin/service/impl/UserInfoServiceImpl.java
@@ -3,21 +3,27 @@ package com.neusoft.wuyemis.admin.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import com.neusoft.wuyemis.admin.mapper.IUserInfoMapper;
 import com.neusoft.wuyemis.admin.model.UserInfoModel;
 import com.neusoft.wuyemis.admin.service.IUserInfoService;
-
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public class UserInfoServiceImpl implements IUserInfoService {
+	
 	@Autowired
 	private IUserInfoMapper userInfoMapper=null;
+
+
+	
+	
 	@Override
 	public void add(UserInfoModel userinfo) throws Exception {
-		userInfoMapper.add(userinfo);
+	
 
 	}
 
@@ -35,7 +41,8 @@ public class UserInfoServiceImpl implements IUserInfoService {
 
 	@Override
 	public List<UserInfoModel> selsetListByAll() throws Exception {
-		// TODO Auto-generated method stub
+
+
 		return null;
 	}
 
